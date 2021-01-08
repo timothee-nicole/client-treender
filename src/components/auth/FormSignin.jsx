9ef5c64c279fc0055b0ae4b589ca42c6ead3bfb1
MaxJGruber <maxjgruber@gmail.com>
2021-01-08 10:44:18 +0100

    pushing for first time

--- src/components/auth/FormSignin.jsx
+++ src/components/auth/FormSignin.jsx
@@ -1,109 +1,114 @@
-import React, { Component } from 'react';
-import { Form, Col, Button } from 'react-bootstrap'
-import apiHandler from '../../api/apihandler'
-import { UserContext } from '../auth/UserContext';
-import {withRouter} from 'react-router-dom'
-
+import React, { Component } from "react";
+import { Form, Col, Button } from "react-bootstrap";
+import apiHandler from "../../api/apihandler";
+import { UserContext } from "../auth/UserContext";
+import { withRouter } from "react-router-dom";
 
 class FormSignin extends Component {
-  static contextType = UserContext
+  static contextType = UserContext;
 
-    state = {
+  state = {};
 
-    }
+  insertGapiScript() {
+    const script = document.createElement("script");
+    script.src = "https://apis.google.com/js/platform.js";
 
-    insertGapiScript() {
-      const script = document.createElement('script');
-      script.src="https://apis.google.com/js/platform.js"
+    script.onload = () => {
+      this.initializeGoogleSignIn();
+    };
+    document.body.appendChild(script);
+  }
 
-      script.onload = () => {
-        this.initializeGoogleSignIn()
-      };
-      document.body.appendChild(script)
-    }
+  initializeGoogleSignIn() {
+    window.gapi.load("auth2", () => {
+      window.gapi.auth2.init({
+        client_id:
+          "205215776462-pl7bogr9ddsla4pkmqrt7p9mr09anrnk.apps.googleusercontent.com",
+      });
+      console.log("API inited");
 
-    initializeGoogleSignIn() {
-      window.gapi.load('auth2', () => {
-        window.gapi.auth2.init({
-          client_id: '205215776462-pl7bogr9ddsla4pkmqrt7p9mr09anrnk.apps.googleusercontent.com'
-        })
-        console.log('API inited');
+      window.gapi.load("signin2", () => {
+        const params = {
+          onsuccess: () => {
+            console.log("User has finished signin in");
+            this.onGoogleSignIn();
+          },
+        };
 
-        window.gapi.load('signin2', () => {
-          const params = {
-            onsuccess: () => {
-              console.log("User has finished signin in")
-              this.onGoogleSignIn()
-            }
-          }
+        window.gapi.signin2.render("loginButton", params);
+      });
+    });
+  }
 
-          window.gapi.signin2.render('loginButton', params)
-        })
+  componentDidMount() {
+    console.log("Loading...");
+
+    this.insertGapiScript();
+  }
+
+  handleChange = (e) => {
+    const key = e.target.name;
+
+    this.setState({
+      [key]: e.target.value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    apiHandler
+      .signin(this.state)
+      .then((data) => {
+        this.context.setUser(data);
+        this.props.history.push("/");
       })
-    }
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    componentDidMount() {
-      console.log('Loading...')
+  onGoogleSignIn = (googleUser) => {
+    console.log(googleUser);
+  };
 
-      this.insertGapiScript();
-
-      
-    }
-
-    handleChange = (e) => {
-        const key = e.target.name;
-
-            this.setState({
-                [key]: e.target.value
-            });
-        
-    }
-
-    handleSubmit = (e) => {
-        e.preventDefault();
-        apiHandler
-          .signin(this.state)
-          .then((data) => {
-            this.context.setUser(data)
-            this.props.history.push('/')
-          })
-          .catch(err => {
-            console.log(err)
-          })
-    }
-
-    onGoogleSignIn = (googleUser) => {
-      console.log(googleUser)
-
-    }
-
-
-    render() {
-        return (
-          <div style={{display: 'flex', flexDirection: "column", alignItems: "center"}}>
-                <Form onSubmit={this.handleSubmit}>
-        <Form.Row>
-          <Form.Group as={Col} controlId="formGridEmail">
-            <Form.Label>Email</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" name="email" onChange={this.handleChange} />
-          </Form.Group>
+  render() {
+    return (
+      <div style={{ margin: "1% 5%" }}>
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Row>
+            <Form.Group as={Col} controlId="formGridEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                name="email"
+                onChange={this.handleChange}
+              />
+            </Form.Group>
           </Form.Row>
           <Form.Row>
-          <Form.Group as={Col} controlId="formGridPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" name="password" onChange={this.handleChange} />
-          </Form.Group>
+            <Form.Group as={Col} controlId="formGridPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                name="password"
+                onChange={this.handleChange}
+              />
+            </Form.Group>
           </Form.Row>
 
           <Button variant="primary" type="submit">
             Submit
           </Button>
-          </Form>
+        </Form>
 
-          <div id="loginButton" style={{margin: "10px"}} data-onsuccess={'this.onGoogleSignIn'}>Signin with Google</div>
-          </div>
-        )
-    }
+        <h5>
+          Don't have an account yet? <Button variant="outline-primary"><a href="/signup">Sign Up</a></Button>
+        </h5>
+      </div>
+    );
+  }
 }
 
-export default withRouter(FormSignin)
+export default withRouter(FormSignin);
