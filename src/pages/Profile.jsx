7381b2385dfa88f94c6104c455b8edc4ecc03583
MaxJGruber <maxjgruber@gmail.com>
2021-02-02 18:19:40 +0100

    Added component to change password, waiting for back end route to work

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -5,6 +5,7 @@ import { faEdit, faCheckCircle } from "@fortawesome/free-solid-svg-icons";
 import { UserContext } from "../components/auth/UserContext";
 import apihandler from "../api/apihandler";
 import withUser from "../components/auth/withUser";
+import EditPasswordForm from "../components/auth/EditPasswordForm";
 
 // Profile page to allow user to change certain lines of data in his/her account
 // Instead of using the same form for CREATE and rendering it with user data in the componentDidMount(), we decided to
@@ -16,9 +17,9 @@ export class Profile extends Component {
   state = {
     userValues: null,
     valuesToUpdate: {
-      address: {}
+      address: {},
     },
-    changePassword: false
+    changePassword: false,
   };
 
   // componentDidMount() {
@@ -27,9 +28,9 @@ export class Profile extends Component {
   // }
   handleChangePassword = () => {
     this.setState({
-      changePassword: !this.state.changePassword
-    })
-  }
+      changePassword: !this.state.changePassword,
+    });
+  };
 
   componentDidUpdate() {
     if (this.context.user && !this.state.userValues) {
@@ -56,7 +57,7 @@ export class Profile extends Component {
     let newuserValues = { ...this.state.userValues };
     newuserValues.address[e.target.name] = e.target.value;
     this.setState({ userValues: newuserValues });
-  }
+  };
 
   handleAddressEdit = (elem) => {
     let updatedValues = { ...this.state.valuesToUpdate };
@@ -64,7 +65,7 @@ export class Profile extends Component {
       ? (updatedValues.address[elem] = true)
       : (updatedValues.address[elem] = false);
     this.setState({ valuesToUpdate: updatedValues });
-  }
+  };
 
   handleAddressConfirm = (elem) => {
     let updatedValues = { ...this.state.valuesToUpdate };
@@ -77,7 +78,7 @@ export class Profile extends Component {
       .editUser(this.state.userValues)
       .then((res) => console.log(res))
       .catch((err) => console.log(err));
-  }
+  };
 
   // FUNCTION to confirm and update lines of user value
   handleConfirm = (elem) => {
@@ -94,122 +95,147 @@ export class Profile extends Component {
   };
 
   transformText = (str) => {
-    let strArr = str.split('')
-    let ixArr = null
+    let strArr = str.split("");
+    let ixArr = null;
     strArr.forEach((elem, i) => {
       if (elem.toUpperCase() === elem) {
-        return ixArr = i
-      }
-      else return elem
-    })
+        return (ixArr = i);
+      } else return elem;
+    });
     if (ixArr) {
-      strArr.splice(ixArr, 0, ' ')
+      strArr.splice(ixArr, 0, " ");
     }
-    strArr[0] = strArr[0].toUpperCase()
-    let newStr = strArr.join('')
-    return newStr
-  }
+    strArr[0] = strArr[0].toUpperCase();
+    let newStr = strArr.join("");
+    return newStr;
+  };
   transformPhoneNumber = (str) => {
-    let phoneArr = str.split('')
+    let phoneArr = str.split("");
     let newArr = phoneArr.map((elem, i) => {
-      if (i !== 0 && i%2 === 0) {
-        return elem = '.' + elem
-      }
-      else return elem
-    })
-    return newArr
-  }
+      if (i !== 0 && i % 2 === 0) {
+        return (elem = "." + elem);
+      } else return elem;
+    });
+    return newArr;
+  };
 
   transformDate = (date) => {
     return date.toString().substring(0, 10);
-
-  }
+  };
   // 1 2 . 3 4 . 5
 
   render() {
-    console.log('state =>>', this.state);
+    console.log("state =>>", this.state);
     // console.log("USER CONTEXT", this.context.user);
     console.log(this.state.userValues && this.state.userValues.address.street);
     return (
-      <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
         <h1>Profile Page</h1>
-        {this.state.changePassword ? <div>EDIT PASSWORD COMPONENT TO GO HERE ICI LA WHOUOUO</div> 
-        : <Table striped bordered hover size="sm">
-          <tbody>
-            {this.state.userValues !== null &&
-              Object.keys(this.state.userValues).map((elem, i) =>
-                // If elem is equal to this, then don't display this on our profile edit page.
-                elem === "isAdmin" ||
-                elem === "agree" ||
-                elem === "_id" ||
-                elem === "profilePicture" ||
-                elem === "__v" ||
-                elem === "address" ||
-                elem === "allOrders" ||
-                elem === "isNL" ? (
-                  ""
-                ) : (
-                  <tr key={i}>
-                    <td>{this.transformText(elem)}</td>
-                    <td>
+        {this.state.changePassword ? (
+          <EditPasswordForm />
+        ) : (
+          <Table striped bordered hover size="sm">
+            <tbody>
+              {this.state.userValues !== null &&
+                Object.keys(this.state.userValues).map((elem, i) =>
+                  // If elem is equal to this, then don't display this on our profile edit page.
+                  elem === "isAdmin" ||
+                  elem === "agree" ||
+                  elem === "_id" ||
+                  elem === "profilePicture" ||
+                  elem === "__v" ||
+                  elem === "address" ||
+                  elem === "allOrders" ||
+                  elem === "isNL" ? (
+                    ""
+                  ) : (
+                    <tr key={i}>
+                      <td>{this.transformText(elem)}</td>
+                      <td>
+                        {this.state.valuesToUpdate[elem] ? (
+                          <form>
+                            <input
+                              name={elem}
+                              type="text"
+                              value={this.state.userValues[elem]}
+                              onChange={this.handleChange}
+                            />
+                          </form>
+                        ) : elem === "phoneNumber" ? (
+                          this.transformPhoneNumber(this.state.userValues[elem])
+                        ) : elem === "birthDate" ? (
+                          this.transformDate(this.state.userValues[elem])
+                        ) : (
+                          this.state.userValues[elem]
+                        )}
+                      </td>
                       {this.state.valuesToUpdate[elem] ? (
-                        <form>
-                          <input
-                            name={elem}
-                            type="text"
-                            value={this.state.userValues[elem]}
-                            onChange={this.handleChange}
-                          />
-                        </form>
+                        <td onClick={() => this.handleConfirm(elem)}>
+                          <FontAwesomeIcon icon={faCheckCircle} />
+                        </td>
                       ) : (
-                        elem === "phoneNumber" ? this.transformPhoneNumber(this.state.userValues[elem]) : elem === "birthDate" ? this.transformDate(this.state.userValues[elem]) : this.state.userValues[elem]
+                        <td onClick={() => this.handleEdit(elem)}>
+                          <FontAwesomeIcon icon={faEdit} />
+                        </td>
                       )}
-                    </td>
-                    {this.state.valuesToUpdate[elem] ? (
-                      <td onClick={() => this.handleConfirm(elem)}>
-                        <FontAwesomeIcon icon={faCheckCircle} />
-                      </td>
-                    ) : (
-                      <td onClick={() => this.handleEdit(elem)}>
-                        <FontAwesomeIcon icon={faEdit} />
-                      </td>
-                    )}
-                  </tr>
-                )
-              )}
-              {this.state.userValues !== null && 
+                    </tr>
+                  )
+                )}
+              {this.state.userValues !== null &&
                 Object.keys(this.state.userValues.address).map((elem, i) => {
-                  return <tr key={i}>
-                    <td>{this.transformText(elem)}</td>
-                    <td>
+                  return (
+                    <tr key={i}>
+                      <td>{this.transformText(elem)}</td>
+                      <td>
+                        {this.state.valuesToUpdate.address[elem] ? (
+                          <form>
+                            <input
+                              name={elem}
+                              type="text"
+                              value={this.state.userValues.address[elem]}
+                              onChange={this.handleAddressChange}
+                            />
+                          </form>
+                        ) : (
+                          this.state.userValues.address[elem]
+                        )}
+                      </td>
                       {this.state.valuesToUpdate.address[elem] ? (
-                        <form>
-                          <input
-                            name={elem}
-                            type="text"
-                            value={this.state.userValues.address[elem]}
-                            onChange={this.handleAddressChange}
-                          />
-                        </form>
+                        <td onClick={() => this.handleAddressConfirm(elem)}>
+                          <FontAwesomeIcon icon={faCheckCircle} />
+                        </td>
                       ) : (
-                        this.state.userValues.address[elem]
+                        <td onClick={() => this.handleAddressEdit(elem)}>
+                          <FontAwesomeIcon icon={faEdit} />
+                        </td>
                       )}
-                    </td>
-                    {this.state.valuesToUpdate.address[elem] ? (
-                      <td onClick={() => this.handleAddressConfirm(elem)}>
-                        <FontAwesomeIcon icon={faCheckCircle} />
-                      </td>
-                    ) : (
-                      <td onClick={() => this.handleAddressEdit(elem)}>
-                        <FontAwesomeIcon icon={faEdit} />
-                      </td>
-                    )}
-                  </tr>
-                    
+                    </tr>
+                  );
                 })}
-          </tbody>
-        </Table>}
-        <div style={{color: "white", backgroundColor: "red", fontWeight: "600", padding: "5px 10px", borderRadius: "10px", width: "fit-content", display: "flex", textAlign: "center"}} onClick={this.handleChangePassword}>Edit Password</div>
+            </tbody>
+          </Table>
+        )}
+        <div
+          style={{
+            color: "white",
+            backgroundColor: "red",
+            fontWeight: "600",
+            padding: "5px 10px",
+            borderRadius: "10px",
+            width: "fit-content",
+            display: "flex",
+            textAlign: "center",
+          }}
+          onClick={this.handleChangePassword}
+        >
+          Edit Password
+        </div>
       </div>
     );
   }
